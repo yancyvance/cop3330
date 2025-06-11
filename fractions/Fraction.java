59639b92bf077d7fc6416b3be53cf3f5afe58f42
Yancy Vance <yancyvance@gmail.com>
2025-06-11 16:32:15 -0400

    Updated Fraction class

--- fractions/Fraction.java
+++ fractions/Fraction.java
@@ -1,5 +1,5 @@
 
-public class Fraction {
+public class Fraction implements Comparable<Fraction> {
 
     private int numerator;
     private int denominator;
@@ -11,13 +11,22 @@ public class Fraction {
     }
     
     public Fraction(int num, int den) {
+        //System.out.println("Accepted: " + num + " " + den);
         setNumerator(num);
         setDenominator(den);
+        //System.out.println("Values Set: " + this.numerator + " " + this.denominator);
         simplify();
+        
+        /*
+        if(this.getDenominator() < 0) {
+            this.setDenominator(-1 * this.getDenominator());
+            this.setNumerator(this.getNumerator() * -1);
+        }
+        */
     }
     
     // setters
-    public void setNumerator(int n) {
+    private void setNumerator(int n) {
         this.numerator = n;
         //simplify();
     }
@@ -27,6 +36,13 @@ public class Fraction {
             d = 1;
         this.denominator = d;
         //simplify();
+        
+        if( this.denominator < 0 ) {
+            this.denominator = this.denominator * -1;
+            this.setNumerator( this.getNumerator() * -1 );
+        }
+        
+        // new Fraction(1, -4);
     }
     
     // getters
@@ -37,15 +53,22 @@ public class Fraction {
     
     public int getDenominator() {
         //simplify();
+        
         return this.denominator;
     }
     
     public String toString() {
+        if( this.getDenominator() == 1 )
+            return String.format("%d", this.getNumerator());
+            
         //return getNumerator() + "/" + getDenominator();
         return String.format("%d/%d", getNumerator(), getDenominator());
     }
     
     public static int findGCD(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        
         if(a == 0)
             return b;
             
@@ -75,23 +98,81 @@ public class Fraction {
         int newNum = this.numerator / gcd;
         int newDen = this.denominator / gcd;
         
-        this.numerator = newNum;
-        this.denominator = newDen;
-        //setNumerator(newNum);
-        //setDenominator(newDen);
+        //this.numerator = newNum;
+        //this.denominator = newDen;
+        setNumerator(newNum);
+        setDenominator(newDen);
     }
     
     public Fraction duplicate() {
-        return null;
+        //Fraction newFrac = new Fraction();
+        //newFrac.setNumerator( this.getNumerator() );
+        //newFrac.numerator = this.numerator;
+        //newFrac.denominator = this.denominator;
+        //return newFrac;
+        //return null;
+        return new Fraction(this.getNumerator(), this.getDenominator());
     }
     
     public boolean equals(Fraction other) {
-        return false;
+        return this.numerator*other.denominator == this.denominator*other.numerator;
+        
+        //return false;
     }
     
     public int compareTo(Fraction other) {
+        int left = this.numerator*other.denominator;
+        int right = this.denominator*other.numerator;
         
+        if(left < right)
+            return -1;
+        else if(left > right)
+            return 1;        
         return 0;
+    }
+    
+    public Fraction multiply(Fraction other) {
+        int newNum = this.getNumerator() * other.getNumerator();
+        int newDen = this.getDenominator() * other.getDenominator();
+        
+        //Fraction result = new Fraction();
+        return new Fraction(newNum, newDen);
+    }
+    
+    public Fraction getInverse() {
+        return new Fraction(this.getDenominator(), this.getNumerator());
+    }
+    
+    public Fraction divide(Fraction other) {
+        return this.multiply( other.getInverse() );
+    }
+    
+    public Fraction multiply(int num) {
+        return this.multiply( new Fraction(num, 1) );
+    }
+    
+    public Fraction subtract(Fraction other) {
+        //other.setNumerator( other.getNumerator() * -1 );
+        
+        return this.add( other.multiply(-1) );
+    }
+    
+    public double toDecimal() {
+        return (double)this.numerator / this.denominator;
+    }
+    
+    public boolean isNegative() {
+        return this.getNumerator() < 0;
+    }
+    
+    public void updateNumerator(int num) {
+        this.setNumerator(num);
+        this.simplify();
+    }
+    
+    public void updateDenominator(int den) {
+        this.setDenominator(den);
+        this.simplify();
     }
     
 }
--- fractions/Main.java
+++ fractions/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Main {
 
@@ -5,22 +6,36 @@ public class Main {
         String s1 = "1/2";
         String s2 = "2/3";
         
-        Fraction f1 = new Fraction(1, 4);
-        Fraction f2 = new Fraction(2, 4);
-        Fraction f3 = f1.add(f2);
+        Fraction f1 = new Fraction(4, -4);
+        Fraction f2 = new Fraction(3, 4);
+        Fraction f3 = f1.subtract(f2);
         
-        System.out.println(f1);
-        System.out.println(f2);
-        System.out.println(f3);
-                
-        System.out.println(f1);
-        //System.out.println(f1.toString());
-        System.out.println(f3);
-        System.out.println(f1);
-        Fraction res = f1.add(f2);
-        System.out.println(res);
-        System.out.println(f1);       
+        System.out.println(f1); // -1
+        System.out.println(f2); // 3/4
+        System.out.println(f3); // -7/4
         
+        Fraction[] fracs = new Fraction[5];
+        
+        fracs[0] = new Fraction(3, 6);
+        fracs[1] = new Fraction(-7, 4);
+        fracs[2] = new Fraction(2, 3);
+        fracs[3] = new Fraction(-10, 5);
+        fracs[4] = new Fraction(5, 2);
+        
+        // print the array
+        printFractions(fracs); // 1/2 -7/4 2/3 -2 5/2
+        
+        // call built-in sort method
+        Arrays.sort(fracs);
+        
+        // print again
+        printFractions(fracs); // -2 -7/4 1/2 2/3 5/2
+    }
+    
+    public static void printFractions(Fraction[] fracs) {
+        for(int i = 0; i < fracs.length; i++)
+            System.out.print(fracs[i] + " ");
+        System.out.println();
     }
 
 }
