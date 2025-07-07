19dcf5dc976690009cfd7c371aaea7c782aa66d2
Yancy Vance <yancyvance@gmail.com>
2025-07-07 08:53:01 -0400

    Create Homework.java

---
+++ homework/pa2/Homework.java
@@ -0,0 +1,23 @@
+
+class Utility {
+    
+    // TODO: Complete the Utility class
+    
+    
+}
+
+
+class StudentList extends BaseList  {
+    
+    // TODO: Complete the StudentList class
+
+
+}
+
+
+class CourseList extends BaseList {
+    
+    // TODO: Complete the CourseList class
+    
+    
+}
